d00684370eecabc3d5be285bb030a0a77389ce19
arioprima <arioprima2@gmail.com>
2023-10-21 21:23:40 +0700

    feat : create navbar

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavigationBar from '../components/NavigationBar'
+
+const About = () => {
+  return (
+    <div>
+        <NavigationBar />
+        About
+    </div>  
+  )
+}
+
+export default About
