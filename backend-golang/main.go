7e822e3c96cf8dc2ce658e0c44efa2e4b35e0a10
arioprima <arioprima2@gmail.com>
2023-10-24 06:05:51 +0700

    chore(be): setup project backend go

---
+++ backend-golang/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.String(200, "Hello, World!")
+	})
+
+	err := router.Run(":8080")
+	if err != nil {
+		return
+	}
+}
