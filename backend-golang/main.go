d4887f23afe32aeb4c9c0c046055942c2f30ab62
Ario Prima <99822382+arioprima@users.noreply.github.com>
2023-10-24 17:47:53 +0700

    Update main.go

--- backend-golang/main.go
+++ backend-golang/main.go
@@ -12,6 +12,9 @@ func main() {
 	router.GET("/satu", func(ctx *gin.Context) {
 		ctx.String(200, "Hello, satu!")
 	})
+	router.GET("/dua", func(ctx *gin.Context) {
+		ctx.String(200, "Hello, satu!")
+	})
 
 	err := router.Run(":8080")
 	if err != nil {
