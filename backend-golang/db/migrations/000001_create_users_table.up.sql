a4c48e3c9a769ababe639d6fe9b17d168aea1c78
arioprima <arioprima2@gmail.com>
2023-10-24 23:23:57 +0700

    feat : create database user

---
+++ backend-golang/db/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,13 @@
+create table users (
+    user_id uuid primary key,
+    firstname varchar(255) not null,
+    lastname varchar(255),
+    username varchar(255) not null unique,
+    email varchar(255) not null unique,
+    password varchar(255) not null,
+    role enum('admin', 'staff', 'employee') default 'employee',
+    image varchar(255),
+    is_deleted tinyint(1) default 0,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
