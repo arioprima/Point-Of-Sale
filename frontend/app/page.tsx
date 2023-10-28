0e06eeea52b58dfe8c0308e386550304ca096403
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-10-28 23:34:14 +0700

    feat : initialize project

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,16 @@
-import NavigationBar from "./components/NavigationBar";
+import ECommerce from "@/components/Dashboard/E-commerce";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "TailAdmin | Next.js E-commerce Dashboard Template",
+  description: "This is Home Blog page for TailAdmin Next.js",
+  // other metadata
+};
 
 export default function Home() {
   return (
-  <NavigationBar />
-  // atau Navigation()
-  )
+    <>
+      <ECommerce />
+    </>
+  );
 }
