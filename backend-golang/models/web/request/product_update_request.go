40e39196210936be176a25a90c50d54a5033f9ad
arioprima <arioprima2@gmail.com>
2023-10-29 23:25:33 +0700

    refactor: Correct the wrong data type in the entity

--- backend-golang/models/web/request/product_update_request.go
+++ backend-golang/models/web/request/product_update_request.go
@@ -1,15 +1,17 @@
 package request
 
+import "time"
+
 type ProductUpdateRequest struct {
-	ProductId     string  `json:"product_id"`
-	ProductName   string  `json:"product_name" validate:"required"`
-	CategoryId    string  `json:"category_id"`
-	Price         int     `json:"price"`
-	Description   *string `json:"description"`
-	Quantity      int     `json:"quantity"`
-	Condition     string  `json:"condition"`
-	Image         *string `json:"image"`
-	SupplierId    string  `json:"supplier_id"`
-	DateOfArrival string  `json:"date_of_arrival"`
-	ExpiryDate    string  `json:"expiry_date"`
+	ProductId     string    `json:"product_id"`
+	ProductName   string    `json:"product_name" validate:"required"`
+	CategoryId    string    `json:"category_id"`
+	Price         int       `json:"price"`
+	Description   *string   `json:"description"`
+	Quantity      int       `json:"quantity"`
+	Condition     string    `json:"condition"`
+	Image         *string   `json:"image"`
+	SupplierId    string    `json:"supplier_id"`
+	DateOfArrival time.Time `json:"date_of_arrival"`
+	ExpiryDate    time.Time `json:"expiry_date"`
 }
--- backend-golang/models/web/response/product_response.go
+++ backend-golang/models/web/response/product_response.go
@@ -1,17 +1,19 @@
 package response
 
+import "time"
+
 type ProductResponse struct {
-	ProductId     string  `json:"product_id"`
-	ProductName   string  `json:"product_name"`
-	CategoryId    string  `json:"category_id"`
-	Price         int     `json:"price"`
-	Description   *string `json:"description"`
-	Quantity      int     `json:"quantity"`
-	Condition     string  `json:"condition"`
-	Image         *string `json:"image"`
-	SupplierId    string  `json:"supplier_id"`
-	DateOfArrival string  `json:"date_of_arrival"`
-	ExpiryDate    string  `json:"expiry_date"`
-	CreatedAt     string  `json:"created_at"`
-	UpdatedAt     string  `json:"updated_at"`
+	ProductId     string    `json:"product_id"`
+	ProductName   string    `json:"product_name"`
+	CategoryId    string    `json:"category_id"`
+	Price         int       `json:"price"`
+	Description   *string   `json:"description"`
+	Quantity      int       `json:"quantity"`
+	Condition     string    `json:"condition"`
+	Image         *string   `json:"image"`
+	SupplierId    string    `json:"supplier_id"`
+	DateOfArrival time.Time `json:"date_of_arrival"`
+	ExpiryDate    time.Time `json:"expiry_date"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }
