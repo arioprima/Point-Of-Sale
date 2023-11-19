298a07d29f430243dd25af9ea8abb90dac5a6421
arioprima <arioprima2@gmail.com>
2023-11-19 20:25:25 +0700

    feat: create connection to database

--- backend/go-postgres/main.go
+++ backend/go-postgres/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"github.com/arioprima/Point-Of-Sale/tree/main/backend/go-postgres/initializers"
+	_ "github.com/lib/pq"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Println("🚀 Could not load environment variables", err)
+		return
+	}
+
+	db, err := initializers.ConnectDB(&config)
+	if err != nil {
+		log.Println("🚀 Could not connect to database", err)
+		return
+	}
+
+	// Check if db is connected
+	err = db.Ping()
+	if err != nil {
+		log.Println("🚀 Could not ping database", err)
+		return
+	}
+
+	log.Println("🚀 Database connection successful")
+
+	// Close the database connection when done
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Println("🚀 Could not close database connection", err)
+			return
+		}
+	}(db)
+
 }
